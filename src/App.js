841e31d278bce1f067a342f287182f38c7599f58
shohibun <shohibunnajam@gmail.com>
2022-04-22 00:01:37 +0700

    perubahan nama

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Shohibun Najam Ilma
         </p>
         <a
           className="App-link"
